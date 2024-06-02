6fa2d5138add0444a8f81722247cc5dc41e004ac
Joyan9 <83577461+Joyan9@users.noreply.github.com>
2024-06-02 18:22:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from google.cloud import documentai
 import os
 import json
 from PyPDF2 import PdfReader
-
+import requests
 
 # Set the default theme to wide
 st.set_page_config(layout="wide")
@@ -27,18 +27,22 @@ PROJECT_ID = "437222221292"
 LOCATION = "us"
 PROCESSOR_ID = "372c4ab238787a42"
 
+# Title note with center alignment
+st.markdown("""
+    <h4 style="text-align: center;">*Note: This app is currently on Google Cloud's Free Trial, which will end on July 14, 2024.</h4>
+    """, unsafe_allow_html=True)
+
 # Center-align the title of the Streamlit app
 st.markdown('<h1 style="text-align: center;">Bank Statement Analyzer</h1>', unsafe_allow_html=True)
 
-# Some introductory content
-st.write("Welcome to the Bank Statement Analyzer. This tool will help you analyze your bank statements by providing you an interactive dataframe and a few visualisations based on the transactions.")
-
-# Some introductory content
-st.write("Note - This web-app was created for academic purpose only and not for production level. Secondly, the app does not store any data on the servers.")
 # Add some vertical space
 st.write("\n" * 10)  # This will add 5 new lines as space
+# Some introductory content
+st.write("Welcome to the Bank Statement Analyzer. This tool will help you analyze your bank statements by providing you an interactive dataframe and a few visualisations based on the transactions. This web-app was created for academic purpose only and not for production level. Secondly, the app does not store any data on the servers.")
 
-
+# Add some vertical space
+st.write("\n" * 10)  # This will add 5 new lines as space
+    
 # Function to process the PDF online
 @st.cache_data(show_spinner=False)
 def online_process(
@@ -127,7 +131,11 @@ def convert_pdf_to_dataframe(
     unique_header_values = list(unique_header_values)
     
     #remove the empty strings
-    unique_header_values = [sublist for sublist in unique_header_values if all(item != "" for item in sublist) and len(sublist) > 4]
+    unique_header_values = [sublist for sublist in unique_header_values if all(item != "" for item in sublist)]
+    
+    # remove lists with less than 4 items
+    unique_header_values = [sublist for sublist in unique_header_values if len(sublist) > 4]
+    
     st.session_state.selected_table = st.radio(
         "Please Select the table containing the transaction records:",
         unique_header_values,
@@ -186,16 +194,17 @@ def categorize_transaction(description: str) -> str:
         'Investment': ['stocks','clearance','fixed deposit','gold'],
         'Others': []
     }
-
-    description = description.lower()
+    
+    description = str(description).lower()
     for category, keywords in categories.items():
         if any(keyword in description for keyword in keywords):
             return category
     return 'Other'
 
 
-# Function to clean and convert to numeric
 def clean_and_convert_to_numeric(series: pd.Series) -> pd.Series:
+    # Convert series to string to ensure string operations can be applied
+    series = series.astype(str)
     # Remove extraneous characters
     series = series.str.replace(r'[^\d.-]', '', regex=True)
     # Replace multiple dots with a single dot (if necessary)
@@ -208,10 +217,54 @@ def clean_and_convert_to_numeric(series: pd.Series) -> pd.Series:
     numeric_series = numeric_series.fillna(0)
     return numeric_series
 
-
 # Upload PDF file
 uploaded_file = st.file_uploader(
     "Please Upload Your Bank Statement PDF file with less than 15 pages", type="pdf")
+
+# URL of the sample bank statement PDF
+pdf_url = "https://github.com/Joyan9/project_computer_science/raw/main/Sample%20Bank%20Statements/Wells%20Fargo%20Bank%20Sample%20Statement.pdf"
+
+# Function to download the PDF content
+@st.cache_data(show_spinner=False)
+def download_pdf(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        st.error("Failed to download the PDF. Please check the URL.")
+        return None
+
+
+col1, col2 = st.columns(2)
+
+# Button to use the sample bank statement
+sample_pdf_button = col1.button(label='Use Sample Bank Statement', 
+                                key='sample_pdf', 
+                                type="primary",
+                                use_container_width = True)
+sample_file = None
+
+if sample_pdf_button or st.session_state.sample_pdf:
+    pdf_content = download_pdf(pdf_url)
+    if pdf_content:
+        st.success("Sample bank statement loaded successfully.")
+        sample_file = pdf_content
+    else:
+        st.write("Unable to load the sample bank statement. Please try again later.")
+
+# Download the PDF
+pdf_content = download_pdf(pdf_url)
+
+if pdf_content:
+    col2.download_button(
+        label="Download Sample Bank Statement",
+        data=pdf_content,
+        file_name="Wells_Fargo_Bank_Sample_Statement.pdf",
+        mime="application/pdf",
+        use_container_width = True
+    )
+else:
+    st.write("Unable to provide the sample bank statement. Please try again later.")
 
 if uploaded_file is not None:
     # Create the temp directory if it doesn't exist
@@ -289,30 +342,19 @@ if uploaded_file is not None:
                                 )
                                 filtered_df.columns = [
                                     key for key in selected_columns.keys()]
-                               
                                 
                                 
-                                filtered_df.replace({'transaction_date': {"": np.nan}
-                                                     }, inplace=True)
-                                filtered_df.dropna(
-                                    subset=['transaction_date'], inplace=True)
-                                
-                                filtered_df.replace({'account_balance': {"": np.nan}
-                                                     }, inplace=True)
-                                filtered_df.dropna(
-                                    subset=['account_balance'], inplace=True)
-    
-                                filtered_df.replace({'description': {"": np.nan}
-                                                     }, inplace=True)
-                                filtered_df.dropna(
-                                    subset=['description'], inplace=True)
-
-                                # Categorize transactions
-                                filtered_df['category'] = filtered_df['description'].apply(
-                                    categorize_transaction)
-
                                 currency_symbol = st.text_input(
                                     "Enter the currency symbol")
+                               
+                                
+                                #Cleaning begins
+                                filtered_df.replace({'transaction_date': {"": np.nan}}, inplace=True)
+                                filtered_df.replace({'account_balance': {"": np.nan}}, inplace=True)
+                                filtered_df.replace({'description': {"": np.nan}}, inplace=True)
+
+                                # Drop rows only if all columns are NaN
+                                filtered_df.dropna(subset = ['description'], inplace=True)
 
                                 # Categorize transactions
                                 filtered_df['category'] = filtered_df['description'].apply(
@@ -321,8 +363,7 @@ if uploaded_file is not None:
 
                                 # Replace None values with 0 for 'debit' and 'credit' columns
                                 filtered_df.replace({'debit': {"": 0, None: 0, np.nan: 0},
-                                                     'credit': {"": 0, None: 0, np.nan: 0}
-                                                     }, inplace=True)
+                                                     'credit': {"": 0, None: 0, np.nan: 0}}, inplace=True)
                                 
                                 # Apply the function to the 'debit' and 'credit' columns
                                 filtered_df['debit'] = clean_and_convert_to_numeric(filtered_df['debit'])
@@ -414,3 +455,174 @@ if uploaded_file is not None:
                                 
                                 # Display the chart
                                 st.plotly_chart(fig, use_container_width=True)
+                                
+elif sample_file:
+    result_df = convert_pdf_to_dataframe(
+            sample_file,
+            PROJECT_ID,
+            LOCATION,
+            PROCESSOR_ID,
+            0,
+        )
+
+    if result_df is not None:
+        headers = [col[0] for col in result_df.columns]
+
+        st.write("Please map the required columns:")
+        st.session_state.selected_columns['transaction_date'] = st.selectbox(
+            "Select the column header matching with Transaction Date",
+            headers,
+            key='transaction_date',
+            index=None
+        )
+        if st.session_state.selected_columns['transaction_date']:
+            st.session_state.selected_columns['description'] = st.selectbox(
+                "Select the column header matching with Description",
+                headers,
+                key='description',
+                index=None
+            )
+            if st.session_state.selected_columns['description']:
+                st.session_state.selected_columns['debit'] = st.selectbox(
+                    "Select the column header matching with Debit / Withdrawals",
+                    headers,
+                    key='debit',
+                    index=None
+                )
+                if st.session_state.selected_columns['debit']:
+                    st.session_state.selected_columns['credit'] = st.selectbox(
+                        "Select the column header matching with Credit / Deposits",
+                        headers,
+                        key='credit',
+                        index=None
+                    )
+                    if st.session_state.selected_columns['credit']:
+                        st.session_state.selected_columns['account_balance'] = st.selectbox(
+                            "Select the column header matching with Account Balance",
+                            headers,
+                            key='account_balance',
+                            index=None
+                        )
+
+                        if st.session_state.selected_columns['account_balance']:
+                            selected_columns = st.session_state.selected_columns
+                            selected_column_values = list(
+                                selected_columns.values())
+
+                            filtered_df = result_df[selected_column_values].copy(
+                            )
+                            filtered_df.columns = [
+                                key for key in selected_columns.keys()]
+                            
+                            
+                            currency_symbol = st.text_input(
+                                "Enter the currency symbol")
+                           
+                            
+                            #Cleaning begins
+                            filtered_df.replace({'transaction_date': {"": np.nan}}, inplace=True)
+                            filtered_df.replace({'account_balance': {"": np.nan}}, inplace=True)
+                            filtered_df.replace({'description': {"": np.nan}}, inplace=True)
+
+                            # Drop rows only if all columns are NaN
+                            filtered_df.dropna(subset = ['description'], inplace=True)
+
+                            # Categorize transactions
+                            filtered_df['category'] = filtered_df['description'].apply(
+                                categorize_transaction)
+
+
+                            # Replace None values with 0 for 'debit' and 'credit' columns
+                            filtered_df.replace({'debit': {"": 0, None: 0, np.nan: 0},
+                                                 'credit': {"": 0, None: 0, np.nan: 0}}, inplace=True)
+                            
+                            # Apply the function to the 'debit' and 'credit' columns
+                            filtered_df['debit'] = clean_and_convert_to_numeric(filtered_df['debit'])
+                            filtered_df['credit'] = clean_and_convert_to_numeric(filtered_df['credit'])
+                            filtered_df['account_balance'] = clean_and_convert_to_numeric(filtered_df['account_balance'])
+                            # Add another horizontal divider using HTML
+                            st.markdown('<hr style="border:1px solid white">', unsafe_allow_html=True)
+                            
+                            # Add a section header for Data Visualization
+                            st.markdown('<h2 style="text-align: center;">Interactive DataFrame</h2>', unsafe_allow_html=True)
+                            st.write("This section will display your bank statement as an interactive dataframe")
+                            edited_df = st.data_editor(filtered_df,
+                                           column_config={
+                                               "debit": st.column_config.NumberColumn(
+                                                   "Debit / Withdrawals",
+                                                   format=f'{currency_symbol}%d'),
+                                               "credit": st.column_config.NumberColumn(
+                                                   "Credit / Deposits",
+                                                   format=f'{currency_symbol}%d'),
+                                               "account_balance": st.column_config.NumberColumn(
+                                                   "Account Balance",
+                                                   format=f'{currency_symbol}%d'),
+                                               "category": st.column_config.SelectboxColumn(
+                                                   "Category",
+                                                   width = "Large",
+                                                   help="Please select appropriate category for transaction",
+                                                   options=[
+                                                            "Food",
+                                                            "Transport",
+                                                            "Shopping",
+                                                            "Utlity Bills",
+                                                            "Entertainment",
+                                                            "Education",
+                                                            "Investment",
+                                                            "Others"
+                                                        ],
+                                                   required=True
+                                           )
+                                            }
+                                           
+                                                   )
+                            
+                            # Add another horizontal divider using HTML
+                            st.markdown('<hr style="border:1px solid white">', unsafe_allow_html=True)
+                            
+                            # Add a section header for Data Visualization
+                            st.markdown('<h2 style="text-align: center;">Data Visualisation</h2>', unsafe_allow_html=True)
+
+
+                            # DATA VISUALISATION
+                            # Chart 1: Debit vs Credit
+                            # Total debit and credit
+                            total_debit = filtered_df['debit'].sum()
+                            total_credit = filtered_df['credit'].sum()
+                            
+                            # Define data and labels
+                            labels = [f'Total Debit ({currency_symbol}{int(total_debit):,})',
+                                      f'Total Credit ({currency_symbol}{int(total_credit):,})']
+                            values = [total_debit, total_credit]
+                            
+                            # Define colors
+                            colors = ['#ff9999', '#66b3ff']
+                            
+                            # Create the doughnut chart
+                            fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='percent', hoverinfo='label+percent',
+                         marker=dict(colors=colors, line=dict(color='white', width=2)),
+                         hole=0.4)])
+                            
+                            fig.update_layout(title='#1 Income to Spend Ratio', title_font_size=18, title_font_weight='bold', title_x=0.0)
+                            
+                            # Show the pie chart in Streamlit
+                            st.plotly_chart(fig, use_container_width=True)
+    
+                            
+                            # Chart 2: Spend by Category
+                            # Grouping by category and summing up the debit values
+                            category_debit_sum = edited_df.groupby('category')['debit'].sum().reset_index()
+                            
+                            # Create the bar chart
+                            fig = px.bar(category_debit_sum, x='debit', y='category', orientation='h', 
+                                         labels={'debit': 'Total Debit', 'category': 'Category'},
+                                         text=category_debit_sum['debit'].astype(int).apply(lambda x: f"{currency_symbol}{x:,.0f}"))
+                            
+                            # Customize the layout
+                            fig.update_layout(title='#2 Total Debit by Category', xaxis_title="Amount Spent", yaxis_title="Category")
+                            
+                            # Make the text bolder
+                            fig.update_traces(textfont=dict(weight='bold'))
+                            
+                            # Display the chart
+                            st.plotly_chart(fig, use_container_width=True)
